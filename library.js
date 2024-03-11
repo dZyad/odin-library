5332e76de71bfcef0e46b0ea91272b1ffe329051
dand <79340018+dZyad@users.noreply.github.com>
2024-03-11 11:43:53 -0300

    Add pet library and create pets in screen

--- library.js
+++ library.js
@@ -1,10 +1,22 @@
 const myPets = [];
 
-function Pet(name, animal, isAdopted) {
-  this.name = name;
-  this.animal = animal;
-  this.isAdopted = isAdopted;
+class Pet {
+    constructor(name, animal, isAdopted) {
+        this.name = name;
+        this.animal = animal;
+        this.isAdopted = isAdopted;
+    }
 }
+
+function addPetToMyPets(name, animal, isAdopted = false) {
+    const pet =  new Pet(name, animal, isAdopted);
+    myPets.push(pet);
+}
+
+addPetToMyPets("Mixtsu", "Cat", true);
+addPetToMyPets("Pompi", "Cat", true);
+addPetToMyPets("Flora", "Cat", true);
+addPetToMyPets("Bongo", "Dog", false);
 
 function addRedSquare() {
     let newSquare = document.createElement('div');
@@ -12,3 +24,31 @@ function addRedSquare() {
 
     mainContainer.appendChild(newSquare);
 }
+
+function addPetCard(pet) {
+    const mainContainer = document.querySelector('main');
+    const newCard = document.createElement('div');
+    newCard.className = 'card';
+
+    const nameField = document.createElement('p');
+    const animalField = document.createElement('p');
+    const adoptField = document.createElement('p');
+
+    nameField.textContent = `Name: ${pet.name}`;
+    animalField.textContent = `Animal: ${pet.animal}`;
+    adoptField.textContent = pet.isAdopted ? 'Family member' : 'In transit';
+
+    newCard.appendChild(nameField);
+    newCard.appendChild(animalField);
+    newCard.appendChild(adoptField);
+
+    mainContainer.appendChild(newCard);
+}
+
+function addActualPets() {
+    for (const pet of myPets) {
+        addPetCard(pet);
+    }
+}
+
+addActualPets();
